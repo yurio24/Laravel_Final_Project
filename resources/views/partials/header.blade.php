4d2bab4bcfe44e79e001fccfe01f8f6a2347b0fe
yurio24 <lestermillaresadang@gmail.com>
2024-04-15 21:12:50 +0800

    Initail commit

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel Demo</title>
     <h1>This is Ulo</h1>
